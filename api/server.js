c07b399cbe6c484f782de3e999fbad5a52b3e060
shirisha9666 <kommushirisha9666@gmail.com>
2025-05-19 22:58:11 +0530

    test-1

--- api/server.js
+++ api/server.js
@@ -1,50 +1,49 @@
 import express from "express";
-import dotenv from "dotenv"
-import authRoutes from "./routes/authRoutes.js"
-import matchRoutes from "./routes/matchRoutes.js"
-import messageRoutes from "./routes/messageRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import matchRoutes from "./routes/matchRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { connectDB } from "./config/db.js";
 import cookieParser from "cookie-parser";
-import cors from "cors"
-import {createServer} from "http"
+import cors from "cors";
+import { createServer } from "http";
 import { initializeSocket } from "./socket/socket.server.js";
 import path from "path";
 dotenv.config();
 
-const app=express();
-const port=process.env.PORT || 5000
-const httpServar=createServer(app)
-const __dirname=path.resolve()
-initializeSocket(httpServar)
-app.use(express.json({limit:'10mb'}))
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+const app = express();
+const port = process.env.PORT || 5000;
+const httpServar = createServer(app);
+const __dirname = path.resolve();
+initializeSocket(httpServar);
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-app.use(cookieParser())
-app.use(cors({
-    origin:process.env.CLIENT_URL,
-    credentials:true
-}))
-
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 // routes
-app.use("/api/auth",authRoutes);
-app.use("/api/users",userRoutes);
-app.use("/api/matches",matchRoutes);
-app.use("/api/messages",messageRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/matches", matchRoutes);
+app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"/client/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("*",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,"client","dist","index.html"))
-})
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "dist", "index.html"));
+  });
 }
-console.log("__dirname:", __dirname);
-console.log("Serving from:", path.join(__dirname, "/client/dist"));
 
 
-httpServar.listen(port,()=>{
-    console.log(`server stated at this ${port}`)
-    connectDB()
-})
+httpServar.listen(port, () => {
+  console.log(`server stated at this ${port}`);
+  connectDB();
+});
