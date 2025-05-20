25daaa1de05b5fc2581c5ab5c34b888747ad9ce7
shirisha9666 <kommushirisha9666@gmail.com>
2025-05-20 14:13:45 +0530

    chnage-cloudinary-api-key

--- api/config/cloudinary.js
+++ api/config/cloudinary.js
@@ -5,6 +5,6 @@ dotenv.config()
 cloudinary.config({
     cloud_name:process.env.Cloudinary_CLOUD_NAME,
     api_key:process.env.Cloudinary_API_KEY,
-    api_secret:process.env.Cloudinary_API_Secret
+    api_secret:process.env.Cloudinary_API_SECRET
 })
 export default cloudinary
